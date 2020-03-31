6dade3fbb70d8a7a1d41de09d18cd7d4b5af0ee4
yanis-aa <60885503+yanis-aa@users.noreply.github.com>
2020-03-31 12:31:07 +0200

    Add files via upload

---
+++ Regles.java
@@ -0,0 +1,13 @@
+package com.example.projetandroid;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class Regles extends AppCompatActivity {
+
+
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.regles);
+    }
+}
